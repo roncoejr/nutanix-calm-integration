938f0b2d2691e11a8a3f021e8f808c3e141a5822
Ronald Coe, Jr <rcoe@miosys.org>
2021-12-24 16:46:18 -0500

    Fix some PHP stuff to get values to show when rendered.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<script language="Javascript" src="js/calm-int.js"></script>
-	<link rel="stylesheet" src="">
+	<link rel="stylesheet" type="text/css" href="">
 	<?php
 		$json_backend = file_get_contents('backend.json');
 		$json_config = file_get_contents('config.json');
@@ -17,15 +17,15 @@
 			<tr><th></th><th></th></tr>
 			<tr><td>Application Name:</td><td><input id="app_name" name="app_name" type="text" width="55"></td></tr>
 			<tr><td>Application Description:</td><td><input id="app_description" name="app_description" type="text" width="255" height="15"></td></tr>
-			<tr><td><input id="btn_clear" name="btn_clear" type="button" value="CLEAR"></td><td><input id="btn_submit" name="btn_submit" type="button" onclick="m_launchApplication(0)" value="SUBMIT"></tr>
+			<tr><td><input id="btn_clear" name="btn_clear" type="button" value="CLEAR"></td><td><input id="btn_submit" name="btn_submit" type="button" value="SUBMIT" onclick="m_launchApplication(0)"></td></tr>
 			<tr><td><div id="infoPanel" name="infoPanel"></div></td></tr>
 		</table>
-		<input id="app_blueprint_uuid" name="app_blueprint_uuid" type="hidden" value="<?php $m_config->uuid ?>">
-		<input id="app_blueprint_name" name="app_blueprint_name" type="hidden" value="<?php $m_config->name ?>">
+		<input id="app_blueprint_uuid" name="app_blueprint_uuid" type="hidden" value="<?php echo $m_config->bp_uuid ?>">
+		<input id="app_blueprint_name" name="app_blueprint_name" type="hidden" value="<?php echo $m_config->bp_name ?>">
 		<input id="app_blueprint_profile" name="app_blueprint_profile" type="hidden" value="app_profile">
-		<input id="hostIP" name="hostIP" type="hidden" value="<?php $m_backend->hostIP ?>">
-		<input id="hostPort" name="hostPort" type="hidden" value="<?php $m_backend->hostPort ?>">
-		<input id="hostEndPoint" name="hostEndPoint" type="hidden" value="<?php $m_backend->hostEndPoint ?>">
+		<input id="hostIP" name="hostIP" type="hidden" value="<?php echo $m_backend->hostIP ?>">
+		<input id="hostPort" name="hostPort" type="hidden" value="<?php echo $m_backend->hostPort ?>">
+		<input id="hostEndPoint" name="hostEndPoint" type="hidden" value="<?php echo $m_backend->hostEndPoint ?>">
 		<input id="t_app_profile_name" name="t_app_profile_name" type="hidden" value="app_profile">
 	</form>
 </body>
