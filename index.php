e315ba66674aec03970ee6942c7958e0f5acaa7f
Ronald Coe, Jr <rcoe@miosys.org>
2021-12-24 12:40:50 -0500

    added some html and javascript

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <html>
-<head><title></title></head>
+<head>
+	<link rel="stylesheet" src="">
+	<title></title>
+</head>
 <body>
+	<form action="POST" method="">
+		<table>
+			<tr><th></th></tr>
+			<tr><td>Application Name:</td><td><input type="text" width="55"></td></tr>
+			<tr><td>Application Description:</td><td><input type="text" width="255"></td></tr>
+		</table>
+	</form>
 </body>
 </html>
