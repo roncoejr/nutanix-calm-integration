2d1aba4df0bb2c21417cddfd7dd50fee8528a1bc
Ronald Coe, Jr <rcoe@miosys.org>
2021-12-24 12:57:09 -0500

    further fleshing out details for main interface and configuration options

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <html>
 <head>
+	<script language="Javascript" src=""></script>
 	<link rel="stylesheet" src="">
 	<title></title>
 </head>
 <body>
-	<form action="POST" method="">
-		<table>
-			<tr><th></th></tr>
-			<tr><td>Application Name:</td><td><input type="text" width="55"></td></tr>
-			<tr><td>Application Description:</td><td><input type="text" width="255"></td></tr>
+	<form id="app_form" name="app_form" action="POST" method="">
+		<table id="tbl_one" name="tbl_one">
+			<tr><th></th><th></th></tr>
+			<tr><td>Application Name:</td><td><input id="app_name" name="app_name" type="text" width="55"></td></tr>
+			<tr><td>Application Description:</td><td><input id="app_description" name="app_description" type="text" width="255"></td></tr>
 		</table>
+		<input id="app_blueprint_uuid" name="app_blueprint_uuid" type="hidden" value="">
+		<input id="app_blueprint_name" name="app_blueprint_name" type="hidden" value="">
+		<input id="app_blueprint_profile" name="app_blueprint_profile" type="hidden" value="app_profile">
 	</form>
 </body>
 </html>
